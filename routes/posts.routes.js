22e1f98b14a7ce47eb49fe93a8f8357f1c45a553
kingseungil <flykim5115@gmail.com>
2022-12-26 16:59:43 +0900

    컨트롤러 구현

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const PostController = require('../controllers/posts.controller');
+const postsController = new PostController();
+
+router.get('/', postsController.getPosts);
+router.post('/', postsController.createPost);
+
 module.exports = router;
